f3318456786739e893aa99b9e2b28db5b40714f6
Dongyeong Jeong <dyjeong365@gmail.com>
2023-02-19 15:32:14 +0900

    [Bronze V] Title: 오늘 날짜, Time: 200 ms, Memory: 17276 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+/*
+서울의 오늘 날짜를 "YYYY-MM-DD" 형식으로 출력
+*/
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
