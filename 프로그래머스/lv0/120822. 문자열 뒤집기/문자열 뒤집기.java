7c782587ff0e17ec62880ca9516cc754ce942c94
Dongyeong Jeong <dyjeong365@gmail.com>
2023-02-27 08:42:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 1.14 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,8 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        for (int i = my_string.length() - 1; i>=0; i--){
-            answer = answer + my_string.charAt(i);
-        }
+        StringBuilder sb = new StringBuilder(my_string);
+        answer += sb.reverse();
         return answer;
     }
 }
