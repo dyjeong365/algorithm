2c54fae686b9f5d3066a64d803f4d690fe0cf10c
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-05 00:27:28 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int price) {
+        double answer = 0.0;
+        if (price>=500000) answer = 0.8 * price;
+        else if (price>=300000) answer = 0.9 * price;
+        else if (price>=100000) answer = 0.95 * price;
+        else answer = price;
+        return (int)answer;
+    }
+}
