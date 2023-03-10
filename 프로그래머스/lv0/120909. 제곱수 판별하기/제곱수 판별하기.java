18338c012bacacfe8a1a4855bebed2acc26ddf76
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-11 01:43:16 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int n) {
         
-        for (int i=1; i<=1000; i++){
-            if(Math.pow(i,2) == n) return 1;
-        }
-        return 2;
+        return n % Math.sqrt(n) == 0 ? 1 : 2;
     }
 }
