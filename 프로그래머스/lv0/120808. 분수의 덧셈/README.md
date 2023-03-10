1037bce0ce4f22b0230f6f101913ceac75a451ed
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-10 11:38:38 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.10 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/lv0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808#) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.15 ms
+메모리: 75.2 MB, 시간: 0.10 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,27 +1,25 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = new int[2];
-        
         int i = 2;
+        int denom = 0;
+        int numer = 0;
         
         if(denom1 == denom2){
-            int denom = denom1;
-            int numer = numer1 + numer2;
+            denom = denom1;
+            numer = numer1 + numer2;
         }
         
-        int denom = denom1 * denom2;
-        int numer = (numer1 * denom2) + (numer2 * denom1);
+        denom = denom1 * denom2;
+        numer = (numer1 * denom2) + (numer2 * denom1);
         
         while(i <= denom && i <= numer){
-            if(numer % i == 0 && denom % i == 0){
+            if( (numer % i == 0) && (denom % i == 0) ){
                 denom /= i;
                 numer /= i;
                 continue;
             }
             i++;
         }
-        answer[0] = numer;
-        answer[1] = denom;
-        return answer;
+        return new int[]{numer,denom};
     }
 }
