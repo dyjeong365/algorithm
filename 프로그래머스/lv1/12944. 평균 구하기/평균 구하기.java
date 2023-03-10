9cb4c86a394082dd3c4ae7f22bf023f40dde1b8b
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-10 18:57:12 +0900

    [level 1] Title: 평균 구하기, Time: 2.16 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public double solution(int[] arr) {
         return Arrays.stream(arr)
-            .mapToDouble(el -> el)
             .average()
             .getAsDouble();
     }
