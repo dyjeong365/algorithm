387822ed6551096ab2dc14ca005cc5e6a28f0d89
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-11 01:29:07 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int hp) {
         final int KING = 5;
         final int MAJOR = 3;
-        final int WORKER = 1;
         
         return (hp / KING) + ((hp % KING) / MAJOR) + ((hp % KING) % MAJOR);
     }
