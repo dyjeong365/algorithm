053bd664f22b61fab02cac4ee9063d382032cc34
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-14 22:16:49 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int result = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        while (N > 1) {
-            result *= N;
-            N--;
-        }
-        System.out.println(result);
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(factorial(N));
+
+    }
+
+    public static int factorial(int N) {
+        if (N == 0) return 1;
+        return N * factorial(N - 1);
     }
 }
