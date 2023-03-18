9bf4522534cfc6d401cf3d513378c597a0f5231e
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-18 13:23:33 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 128 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String input = br.readLine();
+            if (input == null) break;
+
+            sb.append(input + "\n");
+        }
+        System.out.print(sb);
+    }
+}
