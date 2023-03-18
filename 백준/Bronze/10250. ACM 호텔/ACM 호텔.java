5f2bebc4edb45ac01216db79c3e861fb001d7683
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-18 20:17:39 +0900

    [Bronze III] Title: ACM 호텔, Time: 148 ms, Memory: 15944 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -11,19 +11,22 @@ public class Main {
 
         for (int i = 0; i < testcase; i++) {
             st = new StringTokenizer(br.readLine());
-            int height = Integer.parseInt(st.nextToken()); //6
-            int width = Integer.parseInt(st.nextToken()); //12
-            int order = Integer.parseInt(st.nextToken()); //8
-            int room = 0;
+            int height = Integer.parseInt(st.nextToken());
+            int width = Integer.parseInt(st.nextToken());
+            int order = Integer.parseInt(st.nextToken());
+            int roomNumber = 0;
 
-            if ((height * width) < order) break;
+            boolean isOverflow = height * width < order;
+            boolean isTop = order % height == 0;
 
-            if (order % height == 0) {
-                room += (order / height) + (height * 100);
-                sb.append(room + "\n");
+            if (isOverflow) break;
+
+            if (isTop) {
+                roomNumber += (order / height) + (height * 100);
+                sb.append(roomNumber + "\n");
             } else {
-                room += ((int) Math.ceil((double) order / height)) + (order % height) * 100;
-                sb.append(room + "\n");
+                roomNumber += ((int) Math.ceil((double) order / height)) + (order % height) * 100;
+                sb.append(roomNumber + "\n");
             }
         }
         System.out.print(sb);
