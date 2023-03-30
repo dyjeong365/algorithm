e0833c0f86bbaba05ae0f340069891f9cce11b6e
Dongyeong Jeong <dyjeong365@gmail.com>
2023-03-31 07:52:01 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 144 ms, Memory: 15844 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] chess = {1, 1, 2, 2, 2, 8};
+
+        for (int i = 0; i < chess.length; i++) {
+            chess[i] -= Integer.parseInt(st.nextToken());
+            sb.append(chess[i] + " ");
+        }
+        System.out.println(sb);
+    }
+}
