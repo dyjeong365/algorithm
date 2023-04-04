e0728008cb1572a414bb31313f4bfc546bd79a0e
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-05 08:53:37 +0900

    [Bronze V] Title: 직사각형, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int width = Integer.parseInt(br.readLine());
+        int height = Integer.parseInt(br.readLine());
+
+        System.out.println(width * height);
+    }
+}
