9382e05133fad4af64063c7747dfe57a2d27480c
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-09 00:22:20 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,17 +1,10 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String answer = "";
-        for(int i=0; i<my_string.length(); i++){
-            if(i == num1){
-                answer = answer + my_string.charAt(num2);
-                continue;
-            }
-            if(i == num2){
-                answer = answer + my_string.charAt(num1);
-                continue;
-            }
-            answer = answer + my_string.charAt(i);
-        }
-        return answer;
+        char[] answer = my_string.toCharArray();
+        
+        answer[num1] = my_string.charAt(num2);
+        answer[num2] = my_string.charAt(num1);
+        
+        return String.valueOf(answer);
     }
 }
