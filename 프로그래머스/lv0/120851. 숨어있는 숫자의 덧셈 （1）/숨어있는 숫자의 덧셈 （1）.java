316fd392ac84c67c68cc8f36e87c880c3e867573
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-17 08:44:43 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.13 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -2,19 +2,11 @@ class Solution {
     public int solution(String my_string) {
         int answer = 0;
         
-        for (int i=0; i<my_string.length(); i++){
-            
-            // char 변수에 저장된 유니코드를 확인 하려면 char 타입 변수를 int 타입 변수에 저장
-            int uniCode = my_string.charAt(i);
-            
-            if(uniCode < 65){
-                
-                /**  
-                 * 유니코드 값이 아닌 숫자 본연의 값을 구해야 한다. 
-                 * 예를들어 char: '1' -> int: 1    
-                 */
-                answer = answer + Character.getNumericValue(uniCode);   
-            }
+        my_string = my_string.replaceAll("[^0-9]","");
+        char[] temp = my_string.toCharArray();
+        
+        for(var number : temp){
+            answer = answer + Character.getNumericValue(number);
         }
         return answer;
     }
