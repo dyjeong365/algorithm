52e05702d02ab4ae1d0c173fe802797c6b898b0c
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-18 08:47:47 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.74 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,14 +2,9 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        my_string = my_string.replaceAll("[a-z]","");
-        final int LENGTH = my_string.length();
-        int[] answer = new int[LENGTH];
-        
-        for(int i=0; i<LENGTH; i++){
-            answer[i] = Character.getNumericValue(my_string.charAt(i));
-        }
-        Arrays.sort(answer);
-        return answer;
+        return Arrays.stream(my_string.replaceAll("[a-z]","").split(""))
+            .mapToInt(Integer :: parseInt)
+            .sorted()
+            .toArray();    
     }
 }
