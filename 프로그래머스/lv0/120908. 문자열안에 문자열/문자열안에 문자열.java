9d9d67740df479b43dd2756f54973100218a8d8e
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-19 07:47:23 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        return str1.indexOf(str2) == -1 || str1.length() < str2.length() ? 2 : 1;
+        return !str1.contains(str2) || str1.length() < str2.length() ? 2 : 1;
     }
 }
