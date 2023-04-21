01241a49201d18afc835f72b774c150dbdfbec9d
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-22 00:29:30 +0900

    [unrated] Title: n 번째 원소까지, Time: 1.74 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -2,6 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        return Arrays.copyOf(num_list, n);
+        return Arrays.stream(num_list).limit(n).toArray();
     }
 }
