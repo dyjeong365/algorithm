e223d1fb26226225cbd9d4318390b15a7805a5a5
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-23 11:28:57 +0900

    [unrated] Title: 카운트 업, Time: 1.69 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,11 +1,7 @@
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = new int[end - start + 1];
-        
-        for(int i=0; i<answer.length; i++){
-            answer[i] = start;
-            start++;
-        }
-        return answer;
+        return IntStream.rangeClosed(start, end).toArray();
     }
 }
