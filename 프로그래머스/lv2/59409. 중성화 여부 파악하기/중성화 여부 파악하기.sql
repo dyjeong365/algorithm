d11de7d3146b7318c9b799cbca53d1d5a229f1f6
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-27 22:37:43 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,3 +1,3 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME, IF(SEX_UPON_INTAKE LIKE 'Intact%', 'X', 'O') AS 중성화
+SELECT ANIMAL_ID, NAME, IF(SEX_UPON_INTAKE REGEXP 'Neutered|Spayed', 'O', 'X') AS 중성화
 FROM ANIMAL_INS;
