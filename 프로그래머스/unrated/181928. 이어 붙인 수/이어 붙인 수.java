a9dcb62eebdce813308e79291a0262bb394e394f
Dongyeong Jeong <dyjeong365@gmail.com>
2023-04-30 12:02:34 +0900

    [unrated] Title: 이어 붙인 수, Time: 9.17 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        
+        for(var num : num_list){
+            if(num % 2 == 0) even += num;
+            else odd += num;
+        }
+        return Integer.parseInt(odd) + Integer.parseInt(even);
+    }
+}
