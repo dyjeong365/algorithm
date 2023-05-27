c16f06b5580f45759996ae13583f932a07b413a6
Dongyeong Jeong <dyjeong365@gmail.com>
2023-05-27 13:12:23 +0900

    [level 0] Title: A로 B 만들기, Time: 4.08 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        return Arrays.equals(Arrays.stream(before.split(""))
+                             .sorted()
+                             .toArray(), 
+                             Arrays.stream(after.split(""))
+                             .sorted()
+                             .toArray()) 
+            ? 1 : 0;
+    }
+}
