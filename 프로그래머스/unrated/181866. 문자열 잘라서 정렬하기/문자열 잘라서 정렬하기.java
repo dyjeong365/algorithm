9a70ce27669f8ad6e3fb49836f4d8b9e74d10d39
Dongyeong Jeong <dyjeong365@gmail.com>
2023-06-04 13:45:15 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 19.90 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myString) {
+        return Arrays.stream(myString.split("x"))
+            .filter(i -> !i.equals(""))
+            .sorted()
+            .toArray(String[]::new);
+    }
+}
