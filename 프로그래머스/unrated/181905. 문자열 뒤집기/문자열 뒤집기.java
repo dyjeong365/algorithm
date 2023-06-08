ade49dbff731410d4f09412cf517200fa7905fa0
Dongyeong Jeong <dyjeong365@gmail.com>
2023-06-08 11:49:05 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder reverse = new StringBuilder();
+        
+        reverse.append(my_string.substring(s, e+1))
+            .reverse();
+        
+        sb.append(my_string.substring(0, s))
+            .append(reverse)
+            .append(my_string.substring(e+1));
+        
+        return sb.toString();
+    }
+}
