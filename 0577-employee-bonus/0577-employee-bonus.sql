6ef3c1ded15c397a9955e6d4921c31c2e8b7eedb
Dongyeong Jeong <dyjeong365@gmail.com>
2023-06-10 12:57:07 +0900

    Time: 1937 ms (55.20%), Space: 0B (100.00%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-SELECT e.name, b.bonus
+SELECT e.name, bonus
 FROM Employee e
 LEFT JOIN Bonus b
 ON e.empId = b.empId
-WHERE b.bonus < 1000 OR b.bonus IS NULL;
+WHERE bonus < 1000 OR bonus IS NULL;
