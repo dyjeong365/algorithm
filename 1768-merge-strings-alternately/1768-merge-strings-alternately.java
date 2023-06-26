d904d4687bcc5c0acf5831d39ea96b0299b6a58a
Dongyeong Jeong <dyjeong365@gmail.com>
2023-06-26 09:22:06 +0900

    Time: 1 ms (77.48%), Space: 40.8 MB (84.85%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,14 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
+        final int LENGTH1 = word1.length();
+        final int LENGTH2 = word2.length();
         StringBuilder merge = new StringBuilder();
-        final int MIN = Math.min(word1.length(), word2.length());
         
-        for(int i=0; i<MIN; i++){
-            merge.append(word1.charAt(i))
-                .append(word2.charAt(i));
+        for(int i=0; i<Math.max(LENGTH1, LENGTH2); i++){
+            if(i < LENGTH1){
+                merge.append(word1.charAt(i));
+            }
+            if(i < LENGTH2){
+                merge.append(word2.charAt(i));
+            }
         }
         
-        merge.append(word1.length() > word2.length() ? word1.substring(MIN) : word2.substring(MIN));  
         return merge.toString();
     }
 }
