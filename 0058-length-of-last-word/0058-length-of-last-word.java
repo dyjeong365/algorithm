4e27ff605550a96d237dcec5ce966aa531822a84
Dongyeong Jeong <dyjeong365@gmail.com>
2023-07-08 16:13:04 +0900

    Time: 0 ms (100%), Space: 40.8 MB (59.46%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int answer = 0;
+        s = s.trim();
+
+        for(int i = s.length()-1; i >= 0; i--){
+            if(s.charAt(i) == ' ') break;
+            
+            answer++;
+        }
+        return answer;
+    }
+}
