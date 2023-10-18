871a4ae1043d0c976499f4d43cdb0ccd0b267fbd
Dongyeong Jeong <dyjeong365@gmail.com>
2023-10-19 08:41:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,22 +4,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int answer = 0;
 
-        System.out.println(minCount(N));
-    }
-
-    private static int minCount(int number) {
-        if (number % 5 == 0) return number / 5;
-
-        int result = number / 5;
-
-        while (result != 0) {
-            int temp = number - result * 5;
-
-            if (temp % 3 == 0) return result + temp / 3;
-            else result--;
+        while (N > 0) {
+            if (N % 5 == 0) {
+                answer += N / 5;
+                N = 0;
+            } else {
+                N -= 3;
+                answer++;
+            }
         }
 
-        return number % 3 == 0 ? number / 3 : -1;
+        if (N < 0) System.out.println(-1);
+        else System.out.println(answer);
     }
 }
