fd2cdbbd1574364950b863895804c8b417cf98d8
Dongyeong Jeong <dyjeong365@gmail.com>
2023-11-03 09:34:33 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 53388 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
+        Deque<Integer> cards = new LinkedList<>();
+        int top = 1;
+
+        for (int i = 1; i <= N; i++) {
+            cards.offer(i);
+        }
+
+        while (cards.size() >= 2) {
+            cards.pollFirst();
+            top = cards.pollFirst();
+            cards.offerLast(top);
+        }
+
+        System.out.println(top);
+    }
+}
