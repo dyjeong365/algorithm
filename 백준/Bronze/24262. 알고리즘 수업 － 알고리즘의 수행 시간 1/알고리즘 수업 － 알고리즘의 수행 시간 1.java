112ab38a3b28023f888f50e405361870c054559a
Dongyeong Jeong <dyjeong365@gmail.com>
2023-11-05 21:46:48 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(1)
+                .append("\n")
+                .append(0);
+
+        System.out.println(sb);
+    }
+}
