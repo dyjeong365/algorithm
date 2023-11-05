ba5900b46966e17f623423aa9a66248b81df9f50
Dongyeong Jeong <dyjeong365@gmail.com>
2023-11-05 21:51:14 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        sb.append((long) Math.pow(n, 2))
+                .append("\n")
+                .append(2);
+
+        System.out.println(sb);
+    }
+}
