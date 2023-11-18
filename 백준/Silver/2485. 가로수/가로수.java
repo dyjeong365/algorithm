d63a14ae441cb2a4c21851d0e2962c73220a541e
Dongyeong Jeong <dyjeong365@gmail.com>
2023-11-18 12:38:59 +0900

    [Silver IV] Title: 가로수, Time: 252 ms, Memory: 23068 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.java
+++ 백준/Silver/2485. 가로수/가로수.java
@@ -1,45 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int N = Integer.parseInt(br.readLine());
-        List<Integer> integerList = new ArrayList<>();
-        int minDistance = 0;
         int n = 0;
+        int gcd = 0;
         int[] streetTrees = new int[N];
 
         for (int i = 0; i < N; i++) {
             streetTrees[i] = Integer.parseInt(br.readLine());
 
             if (i > 0) {
-                integerList.add(streetTrees[i] - streetTrees[i - 1]);
+                int distance = streetTrees[i] - streetTrees[i - 1];
+                gcd = gcd(distance, gcd);
             }
         }
 
-        final int MIN = integerList.stream().mapToInt(Integer::intValue).min().getAsInt();
-
-        for (int i = MIN; i > 0; i--) {
-            boolean flag = true;
-
-            for (int el : integerList) {
-                if (el % i != 0) {
-                    flag = false;
-                    break;
-                }
-            }
-
-            if (flag) {
-                minDistance = i;
-                break;
-            }
-        }
-
-        n = ((streetTrees[N - 1] - streetTrees[0]) / minDistance) + 1;
+        n = ((streetTrees[N - 1] - streetTrees[0]) / gcd) + 1;
         System.out.println(n - streetTrees.length);
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
