ffabe9f7c146301874106ebe8f2e1483ff60abf2
Dongyeong Jeong <dyjeong365@gmail.com>
2023-12-11 13:43:52 +0900

    [Silver III] Title: 파도반 수열, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -3,28 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final long T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        long[] P = new long[101];
+        final int MAX = 101;
+        final long[] P = new long[MAX];
         P[1] = P[2] = P[3] = 1;
 
-        for (long i = 0; i < T; i++) {
-            sb.append(calcP(Integer.parseInt(br.readLine()), P)).append("\n");
+        for (int i = 4; i < MAX; i++) {
+            P[i] = P[i - 3] + P[i - 2];
+        }
+
+        for (int i = 0; i < T; i++) {
+            final int N = Integer.parseInt(br.readLine());
+
+            sb.append(P[N]).append("\n");
         }
 
         System.out.print(sb);
-    }
-
-    private static long calcP(final int N, long[] P) {
-        if (P[N] == 0) {
-            for (int i = 4; i <= N; i++) {
-                P[i] = P[i - 3] + P[i - 2];
-            }
-        }
-
-        return P[N];
     }
 }
