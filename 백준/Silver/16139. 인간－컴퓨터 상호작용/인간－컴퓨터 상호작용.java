f848311da275c6429d720a241cc74cef1ade6498
Dongyeong Jeong <dyjeong365@gmail.com>
2023-12-28 10:40:48 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 740 ms, Memory: 93608 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -9,23 +9,31 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         final String S = br.readLine();
+        final int LENGTH = S.length();
         int q = Integer.parseInt(br.readLine());
+        final int NUMBER_OF_LETTERS = 26;
+
+        int[][] sum = new int[NUMBER_OF_LETTERS][LENGTH + 1];
+
+        for (int i = 0; i < NUMBER_OF_LETTERS; i++) {
+            for (int j = 1; j < LENGTH + 1; j++) {
+                if (S.charAt(j - 1) == 'a' + i) {
+                    sum[i][j] = sum[i][j - 1] + 1;
+                } else {
+                    sum[i][j] = sum[i][j - 1];
+                }
+            }
+        }
 
         for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
-
-            String target = st.nextToken();
+            char target = st.nextToken().charAt(0);
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            int count = 0;
+            int alphabet = target - 'a';
 
-            for (int j = start; j <= end; j++) {
-                if (target.equals(String.valueOf(S.charAt(j)))) {
-                    count++;
-                }
-            }
-
-            sb.append(count).append("\n");
+            sb.append(sum[alphabet][end + 1] - sum[alphabet][start])
+                    .append("\n");
         }
 
         System.out.print(sb);
