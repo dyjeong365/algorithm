98ef2f65f87cab5ecdaa500a492036da83f7c6d3
Dongyeong Jeong <dyjeong365@gmail.com>
2023-12-29 15:35:40 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 1708 ms, Memory: 101184 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -20,39 +20,49 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
 
-            if (a == -1 && a == b && a == c) {
+            if (a == -1 && b == -1 && c == -1) {
                 break;
             }
 
-            sb.append(String.format("w(%d, %d, %d) = %d", a, b, c, w(a, b, c))).append("\n");
+            sb.append(String.format("w(%d, %d, %d) = %d", a, b, c, w(a, b, c)))
+                    .append("\n");
         }
 
         System.out.print(sb);
     }
 
     private static int w(int a, int b, int c) {
+        if (inRange(a, b, c) && result[a][b][c] != 0) {
+            return result[a][b][c];
+        }
+
         if (a <= 0 || b <= 0 || c <= 0) {
             return 1;
         }
 
         if (a > 20 || b > 20 || c > 20) {
             result[20][20][20] = w(20, 20, 20);
-            
+
             return result[20][20][20];
         }
 
-        if (result[a][b][c] != 0) {
-            return result[a][b][c];
-        }
-
         if (a < b && b < c) {
-            result[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+            result[a][b][c] = w(a, b, c - 1)
+                    + w(a, b - 1, c - 1)
+                    - w(a, b - 1, c);
 
             return result[a][b][c];
         }
 
-        result[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        result[a][b][c] = w(a - 1, b, c)
+                + w(a - 1, b - 1, c)
+                + w(a - 1, b, c - 1)
+                - w(a - 1, b - 1, c - 1);
 
         return result[a][b][c];
+    }
+
+    private static boolean inRange(int a, int b, int c) {
+        return a > 0 && a <= 20 && b > 0 && b <= 20 && c > 0 && c <= 20;
     }
 }
