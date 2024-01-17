352d9ca1fb5f4a8b046d061ed8f906be9d909972
Dongyeong Jeong <dyjeong365@gmail.com>
2024-01-18 08:58:00 +0900

    Time: 0 ms (100.00%), Space: 40.5 MB (11.95%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public int tribonacci(int n) {
-        final int MAX = 37;
-        int[] T = new int[MAX + 1];
+        int[] T = new int[n + 3];
         T[2] = T[1] = 1;
         
-        for(int i=3; i<MAX+1; i++) {
+        for(int i=3; i<n+1; i++) {
             T[i] = T[i-3] + T[i-2] + T[i-1];
         }
         
