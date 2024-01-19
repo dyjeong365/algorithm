b1c521492d181b09ebd7b685775e67cde812cf44
Dongyeong Jeong <dyjeong365@gmail.com>
2024-01-19 11:56:12 +0900

    Time: 11 ms (9.72%), Space: 46.7 MB (41.93%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,7 +3,26 @@ class Solution {
         int[] count = new int[n+1];
         
         for(int i=0; i<n+1; i++) {
-            count[i] = Integer.bitCount(i);
+            count[i] = countNumberOfOne(i);
+        }
+        
+        return count;
+    }
+    
+    private int countNumberOfOne(int i) {
+        int count = 0;
+        
+        while(i != 0) {
+            if(i % 2 == 1) {
+                count++;
+            }
+            
+            if(i / 2 == 1) {
+                count++;
+                break;
+            }
+            
+            i /= 2;
         }
         
         return count;
