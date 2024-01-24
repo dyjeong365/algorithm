7699b07cf63faf6af659c9fed7c09307b2e1faa2
Dongyeong Jeong <dyjeong365@gmail.com>
2024-01-24 13:02:32 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (36.81%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,15 +10,17 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode newHead = null;
-        
-        while(head != null) {
-            ListNode next = head.next;
-            head.next = newHead;
-            newHead = head;
-            head = next;
+        return reverse(head, null);
+    }
+    
+    private ListNode reverse(ListNode head, ListNode newHead) {
+        if(head == null) {
+            return newHead;
         }
         
-        return newHead;
+        ListNode next = head.next;
+        head.next = newHead;
+        
+        return reverse(next, head);
     }
 }
