fdd87ab9d90e4664a2acb4e51131c1a52ce75aff
Dongyeong Jeong <dyjeong365@gmail.com>
2024-02-10 12:09:08 +0900

    Time: 1 ms (99.15%), Space: 44.8 MB (50.03%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,19 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> map = new HashMap<>();
+        int[] alphabet = new int[26];
         
         for(var el : magazine.toCharArray()) {
-            map.put(el, map.getOrDefault(el, 0) + 1);
+            alphabet[el - 'a']++;
         }
         
         for(var el : ransomNote.toCharArray()) {
-            if(!map.containsKey(el)) return false;
+            if(alphabet[el - 'a'] == 0) return false;
             
-            if(map.containsKey(el)) {
-                map.put(el, map.get(el) - 1);
-                
-                if(map.get(el) < 0) return false;
-            }
+            alphabet[el - 'a']--;
         }
         
         return true;
