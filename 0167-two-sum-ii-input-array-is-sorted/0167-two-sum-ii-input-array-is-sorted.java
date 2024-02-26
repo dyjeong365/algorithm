85f6954a681d05d890e13a8ab62d496c4930be84
Dongyeong Jeong <dyjeong365@gmail.com>
2024-02-26 09:49:17 +0900

    Time: 3 ms (29.02%), Space: 47.3 MB (20.69%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    return new int[] { i + 1, j + 1 };
-                }
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while (numbers[start] + numbers[end] != target) {
+            if (numbers[start] + numbers[end] > target) {
+                end--;
+            } else if (numbers[start] + numbers[end] < target) {
+                start++;
             }
         }
 
-        return new int[] {};
+        return new int[] { start + 1, end + 1 };
     }
 }
