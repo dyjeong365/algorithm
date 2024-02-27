9b63239dc55019e61ee3ae4d8a9b3a40d83086eb
Dongyeong Jeong <dyjeong365@gmail.com>
2024-02-27 13:25:31 +0900

    Time: 5 ms (61.87%), Space: 44.4 MB (85.44%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,55 +1,23 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<String, Integer> map = new HashMap<>();
-        String[] symbols = { "I", "V", "X", "L", "C", "D", "M" };
-        int[] values = { 1, 5, 10, 50, 100, 500, 1000 };
+        final int LENGTH = s.length();
 
-        for (int i = 0; i < symbols.length; i++) {
-            map.put(symbols[i], values[i]);
-        }
-
-        Queue<String> queue = new LinkedList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         int answer = 0;
 
-        for (var el : s.split("")) {
-            queue.offer(el);
-        }
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
-        while (!queue.isEmpty()) {
-            String cur = queue.poll();
+        for (int i = 0; i < LENGTH; i++) {
+            int cur = map.get(s.charAt(i));
+            int next = i == LENGTH - 1 ? Integer.MIN_VALUE : map.get(s.charAt(i + 1));
 
-            if (!queue.isEmpty()) {
-                String next = queue.peek();
-
-                if (cur.equals("I")) {
-                    if (next.equals("V") || next.equals("X")) {
-                        answer += map.get(next) - map.get(cur);
-                        queue.poll();
-                        System.out.println("I: " + answer);
-                    } else
-                        answer += map.get(cur);
-                }
-
-                else if (cur.equals("X")) {
-                    if (next.equals("L") || next.equals("C")) {
-                        answer += map.get(next) - map.get(cur);
-                        queue.poll();
-                        System.out.println("X: " + answer);
-                    } else
-                        answer += map.get(cur);
-                }
-
-                else if (cur.equals("C")) {
-                    if (next.equals("D") || next.equals("M")) {
-                        answer += map.get(next) - map.get(cur);
-                        System.out.println("C: " + answer);
-                        queue.poll();
-                    } else
-                        answer += map.get(cur);
-                } else
-                    answer += map.get(cur);
-            } else
-                answer += map.get(cur);
+            answer = cur < next ? answer - cur : answer + cur;
         }
 
         return answer;
