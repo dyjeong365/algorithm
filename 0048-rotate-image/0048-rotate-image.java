9e455755badefb1e459cec61e487964466d3ff56
Dongyeong Jeong <dyjeong365@gmail.com>
2024-02-29 14:56:39 +0900

    Time: 0 ms (100%), Space: 41.8 MB (87.55%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        final int n = matrix.length;
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j >= 0; j--) {
+                queue.offer(matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = queue.poll();
+            }
+        }
+    }
+}
