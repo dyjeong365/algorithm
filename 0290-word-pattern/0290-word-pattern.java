99152ca8f926ed88969221ae3a528cf8180570df
Dongyeong Jeong <dyjeong365@gmail.com>
2024-03-02 13:16:48 +0900

    Time: 0 ms (100%), Space: 41 MB (90.09%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,34 +1,27 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] splitedS = s.split(" ");
-        Map<String, String> map = new HashMap<>();
+        Map<Character, String> map = new HashMap<>();
 
         if (pattern.length() != splitedS.length) {
             return false;
         }
 
         for (int i = 0; i < pattern.length(); i++) {
-            String key = "" + pattern.charAt(i);
+            Character key = pattern.charAt(i);
             String value = splitedS[i];
 
-            if (map.containsKey(key)) {
-                if (!map.get(key).equals(value)) {
-                    return false;
-                }
-            }
-
-            else if (map.containsValue(value)) {
-                if (!map.containsKey(key)) {
+            if (!map.containsKey(key)) {
+                if (map.containsValue(value)) {
                     return false;
                 }
 
-                else if (!map.get(key).equals(value)) {
-                    return false;
-                }
-            }
-
-            else
                 map.put(key, value);
+            }
+
+            else if (!map.get(key).equals(value)) {
+                return false;
+            }
         }
 
         return true;
