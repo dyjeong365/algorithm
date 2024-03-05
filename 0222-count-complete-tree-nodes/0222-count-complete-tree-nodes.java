d89150e1c768e6ff0ebd44aff85392b4175d554d
Dongyeong Jeong <dyjeong365@gmail.com>
2024-03-05 09:55:10 +0900

    Time: 0 ms (100%), Space: 47.6 MB (53.65%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,15 +16,10 @@
 class Solution {
 
     public int countNodes(TreeNode root) {
-        int count = 0;
-
         if (root == null) {
-            return count;
+            return 0;
         }
 
-        count++;
-        count += countNodes(root.left) + countNodes(root.right);
-
-        return count;
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
