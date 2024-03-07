5fc5fed0bc63f36ae5316d97ece53ff89311e25f
Dongyeong Jeong <dyjeong365@gmail.com>
2024-03-07 14:23:03 +0900

    Time: 0 ms (100%), Space: 43.9 MB (97.05%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,27 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Map<Integer, Integer> map = new TreeMap<>();
+        int i = 0;
 
-        for (var el : nums) {
-            if (map.containsKey(el)) {
-                if (map.get(el) != 2) {
-                    map.put(el, map.get(el) + 1);
-                }
-            } else
-                map.put(el, 1);
-        }
-
-        int idx = 0;
-
-        for (var el : map.entrySet()) {
-            int key = el.getKey();
-            int value = el.getValue();
-
-            while (value-- > 0) {
-                nums[idx++] = key;
+        for (var num : nums) {
+            if (i < 2 || num > nums[i - 2]) {
+                nums[i++] = num;
             }
         }
 
-        return idx;
+        return i;
     }
 }
