5bb5d8b1e7837a7a874a69a5b710413d78e5ab3c
Dongyeong Jeong <dyjeong365@gmail.com>
2024-03-10 11:45:26 +0900

    Time: 0 ms (100%), Space: 44.4 MB (48.95%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> set = new HashSet<>();
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while (head != null) {
-            if (set.contains(head)) {
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
                 return true;
             }
-
-            set.add(head);
-            head = head.next;
         }
 
         return false;
