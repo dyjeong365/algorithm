0f9ea843de7641d94e6a153ad218f4114c89a3d9
Dongyeong Jeong <dyjeong365@gmail.com>
2024-04-01 22:55:20 +0900

    Time: 1 ms (100%), Space: 41.6 MB (74.52%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,25 +1,15 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
+        char c = 0;
 
         for(var el : s.toCharArray()) {
-            map.put(el, map.getOrDefault(el, 0) + 1);
+            c ^= el;
         }
 
         for(var el : t.toCharArray()) {
-            if(map.containsKey(el)) {
-                map.put(el, map.get(el) - 1);
-            } else {
-                return el;
-            }
+            c ^= el;
         }
 
-        for(var entrySet : map.entrySet()) {
-            if(entrySet.getValue() == -1) {
-                return entrySet.getKey();
-            }
-        }
-
-        return ' ';
+        return c;
     }
 }
