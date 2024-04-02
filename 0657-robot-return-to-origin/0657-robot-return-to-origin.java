51a1fa68283ef77df81a08003191a0224b0a76f5
Dongyeong Jeong <dyjeong365@gmail.com>
2024-04-03 08:41:00 +0900

    Time: 4 ms (97.64%), Space: 44.4 MB (60.36%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+
+        for (var move : moves.toCharArray()) {
+            if (move == 'R') {
+                x++;
+            }
+
+            else if (move == 'L') {
+                x--;
+            }
+
+            else if (move == 'U') {
+                y++;
+            }
+
+            else {
+                y--;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
