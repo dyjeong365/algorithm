d579fc5d0fcf778cb4c508ff4bdcc01df2383aef
Dongyeong Jeong <dyjeong365@gmail.com>
2024-04-07 20:15:32 +0900

    Time: 0 ms (100%), Space: 45.7 MB (44.43%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,22 +3,22 @@ class Solution {
         int start = 0;
         int end = nums.length - 1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
+        return recursive(nums, target, start, end);
+    }
 
-            if (nums[mid] < target) {
-                start = mid + 1;
-            }
-
-            else if (nums[mid] > target) {
-                end = mid - 1;
-            }
-
-            else {
-                return mid;
-            }
+    private static int recursive(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            return -1;
         }
 
-        return -1;
+        int mid = (start + end) / 2;
+
+        if (target > nums[mid]) {
+            return recursive(nums, target, mid + 1, end);
+        } else if (target < nums[mid]) {
+            return recursive(nums, target, start, mid - 1);
+        } else {
+            return mid;
+        }
     }
 }
