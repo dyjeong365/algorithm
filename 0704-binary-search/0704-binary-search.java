d2e17c4bbafadb502ca830d52bf1be9d04b2f4f2
Dongyeong Jeong <dyjeong365@gmail.com>
2024-04-07 20:09:53 +0900

    Time: 0 ms (100%), Space: 46 MB (7.25%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,10 +2,9 @@ class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        int mid = 0;
 
         while (start <= end) {
-            mid = (start + end) / 2;
+            int mid = (start + end) / 2;
 
             if (nums[mid] < target) {
                 start = mid + 1;
