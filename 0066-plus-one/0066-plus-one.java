5ec0955fe1384f8dfc8c2eaaf01b9aedb34dc3e3
Dongyeong Jeong <dyjeong365@gmail.com>
2024-04-10 12:38:52 +0900

    Time: 0 ms (100%), Space: 41.8 MB (28.16%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -10,6 +10,10 @@ class Solution {
             digits[i] = 0;
         }
 
+        /*
+         * by default new array values are set to -->0 only
+         * thus just changed first value of array to 1 and return the array
+         */
         digits = new int[digits.length + 1];
         digits[0] = 1;
 
