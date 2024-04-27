685c173835c11261bf97daa7b695c27e8c076639
Dongyeong Jeong <dyjeong365@gmail.com>
2024-04-27 09:13:03 +0900

    Time: 1 ms (96.22%), Space: 42.4 MB (9.74%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -1,12 +1,19 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-        int sum = 0;
+        int answer = 0;
 
+        /*
+         * "BCM" = (2 x 26 x 26) + (3 x 26) + (13)
+         * 
+         * "B" = 2
+         * "BC" = (2)26 + 3
+         * "BCM" = (2(26) + 3)26 + 13
+         */
         for (var el : columnTitle.toCharArray()) {
-            sum *= 26;
-            sum += el - 'A' + 1;
+            answer *= 26;
+            answer += el - 'A' + 1;
         }
 
-        return sum;
+        return answer;
     }
 }
