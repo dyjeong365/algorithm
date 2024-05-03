be547d0d9194f3934811419a11fb5a9adf8f0480
Dongyeong Jeong <dyjeong365@gmail.com>
2024-05-03 13:40:27 +0900

    Time: 0 ms (100%), Space: 40.8 MB (62.11%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.java
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.java
@@ -5,14 +5,12 @@ class Solution {
         // L, W - small as possible
         // return [L, W]
 
-        for (int i = (int) Math.sqrt(area); i <= area; i++) {
+        for (int i = (int) Math.sqrt(area); i >= 1; i--) {
             if (area % i == 0) {
                 int length = i;
                 int width = area / i;
 
-                if (length >= width) {
-                    return new int[] { length, width };
-                }
+                return new int[] { Math.max(length, width), Math.min(length, width) };
             }
         }
 
