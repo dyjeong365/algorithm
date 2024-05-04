da001c239b6278097634b98a6f5106c4e7882550
Dongyeong Jeong <dyjeong365@gmail.com>
2024-05-04 12:47:30 +0900

    Time: 2 ms (27.14%), Space: 40.8 MB (77.05%) - LeetHub

--- 0504-base-7/0504-base-7.java
+++ 0504-base-7/0504-base-7.java
@@ -1,5 +1,15 @@
 class Solution {
     public String convertToBase7(int num) {
-        return Integer.toString(num, 7);
+        StringBuilder sb = new StringBuilder();
+        final String MINUS = "-";
+
+        while (Math.abs(num) >= 7) {
+            sb.append(num % 7);
+            num /= 7;
+        }
+
+        sb.append(num);
+
+        return num < 0 ? MINUS + sb.reverse().toString().replace("-", "") : sb.reverse().toString();
     }
 }
