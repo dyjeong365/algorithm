806e68bd3495d6ed4f75e01e6ec8c77f32894ae4
Dongyeong Jeong <dyjeong365@gmail.com>
2024-05-10 10:42:13 +0900

    [D1] Title: 연월일 달력, Time: 172 ms, Memory: 27,424 KB -BaekjoonHub

--- SWEA/D1/2056. 연월일 달력/연월일 달력.java
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.java
@@ -1,19 +1,13 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Solution {
-    static List<String> months;
     static int[] days;
 
     public static void main(String[] args) {
         final String DELIMITER = "/";
         Scanner sc = new Scanner(System.in);
         final int T = sc.nextInt();
-        months = Arrays.asList
-                (
-                        "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"
-                );
+
         days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
         for (int i = 0; i < T; i++) {
@@ -31,7 +25,7 @@ public class Solution {
     }
 
     private static boolean validateDate(String month, String day) {
-        if (months.contains(month)) {
+        if (Integer.parseInt(month) > 0 && Integer.parseInt(month) < 12) {
             return Integer.parseInt(day) <= days[Integer.parseInt(month)];
         }
 
