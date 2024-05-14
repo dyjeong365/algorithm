dee3e8f7a16bc821cbe0f0be3907f0ec4acfea73
Dongyeong Jeong <dyjeong365@gmail.com>
2024-05-14 10:19:30 +0900

    [D2] Title: 중간 평균값 구하기, Time: 104 ms, Memory: 23,856 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -10,24 +12,25 @@ public class Solution {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         final int T = Integer.parseInt(br.readLine());
-        int[] nums = new int[10];
+        List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            int idx = 0;
 
             while (st.hasMoreTokens()) {
-                nums[idx++] = Integer.parseInt(st.nextToken());
+                list.add(Integer.parseInt(st.nextToken()));
             }
 
-            Arrays.sort(nums);
+            Collections.sort(list);
+
             double sum = 0;
 
             for (int j = 1; j < 9; j++) {
-                sum += nums[j];
+                sum += list.get(j);
             }
 
             sb.append("#" + (i + 1) + " " + Math.round(sum / 8) + "\n");
+            list.clear();
         }
 
         System.out.print(sb);
