6173bcb54f4a53537cd36869b3113f5b536d1816
Dongyeong Jeong <dyjeong365@gmail.com>
2024-05-30 08:27:26 +0900

    Time: 1 ms (99%), Space: 42.8 MB (44.52%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.java
@@ -3,6 +3,7 @@ class Solution {
         int answer = 0;
 
         for (var num : nums) {
+            // All x such that 10^k <= x < 10^k+1 have k+1 digits where k >= 0
             int count = (int) Math.floor(Math.log10(num) + 1);
 
             if (count % 2 == 0) {
