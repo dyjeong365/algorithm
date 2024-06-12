fc7b59b7d4abf775dc70572b7afaf5a9d4fe8434
Dongyeong Jeong <dyjeong365@gmail.com>
2024-06-13 00:30:50 +0900

    Time: 0 ms (100%), Space: 43.9 MB (98.27%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -2,11 +2,11 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         final int LEN = nums.length;
         int x = 0;
-        int y = nums.length / 2;
+        int y = n;
         int[] ans = new int[LEN];
 
         for (int i = 0; i < LEN; i++) {
-            ans[i] = i % 2 == 0 ? nums[x++] : nums[y++];
+            ans[i] = (i % 2 == 0) ? nums[x++] : nums[y++];
         }
 
         return ans;
