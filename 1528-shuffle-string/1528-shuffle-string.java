5bf33086764ce02d81b5ac2c45601b3fef8505d2
Dongyeong Jeong <dyjeong365@gmail.com>
2024-06-17 19:37:09 +0900

    Time: 1 ms (71.89%), Space: 44.6 MB (33.51%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,11 +1,14 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
         final int LEN = s.length();
-        StringBuilder sb = new StringBuilder(s);
+        StringBuilder sb = new StringBuilder();
+        char[] splitedS = new char[LEN];
 
         for (int i = 0; i < LEN; i++) {
-            sb.setCharAt(indices[i], s.charAt(i));
+            splitedS[indices[i]] = s.charAt(i);
         }
+
+        sb.append(splitedS);
 
         return sb.toString();
     }
