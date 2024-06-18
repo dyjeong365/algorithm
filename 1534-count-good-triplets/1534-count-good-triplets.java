671d47929fc18ad51be82f89012d39a27698e868
Dongyeong Jeong <dyjeong365@gmail.com>
2024-06-19 08:14:17 +0900

    Time: 10 ms (88.56%), Space: 41.3 MB (29.59%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.java
+++ 1534-count-good-triplets/1534-count-good-triplets.java
@@ -5,10 +5,13 @@ class Solution {
 
         for (int i = 0; i < LEN - 2; i++) {
             for (int j = i + 1; j < LEN - 1; j++) {
-                for (int k = j + 1; k < LEN; k++) {
-                    if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b
-                            && Math.abs(arr[i] - arr[k]) <= c) {
-                        cnt++;
+                if (Math.abs(arr[i] - arr[j]) <= a) {
+                    for (int k = j + 1; k < LEN; k++) {
+                        if (Math.abs(arr[j] - arr[k]) <= b) {
+                            if (Math.abs(arr[i] - arr[k]) <= c) {
+                                cnt++;
+                            }
+                        }
                     }
                 }
             }
