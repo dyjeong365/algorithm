fac60a203a003bbc413f9f2f257af6972a388b6f
Dongyeong Jeong <dyjeong365@gmail.com>
2024-06-26 12:11:41 +0900

    Time: 4 ms (69.72%), Space: 44.6 MB (59.08%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
         List<Integer> ans = new ArrayList<>();
+        int[] nums = new int[101];
 
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> set2 = new HashSet<>();
         Set<Integer> set3 = new HashSet<>();
-        Map<Integer, Integer> map = new HashMap<>();
 
         for (var num1 : nums1) {
             set1.add(num1);
@@ -20,20 +20,20 @@ class Solution {
         }
 
         for (var num1 : set1) {
-            map.put(num1, map.getOrDefault(num1, 0) + 1);
+            nums[num1]++;
         }
 
         for (var num2 : set2) {
-            map.put(num2, map.getOrDefault(num2, 0) + 1);
+            nums[num2]++;
         }
 
         for (var num3 : set3) {
-            map.put(num3, map.getOrDefault(num3, 0) + 1);
+            nums[num3]++;
         }
 
-        for (var entry : map.entrySet()) {
-            if (entry.getValue() > 1) {
-                ans.add(entry.getKey());
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > 1) {
+                ans.add(i);
             }
         }
 
