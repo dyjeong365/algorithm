f6047460102010cd8a97ad1ca786de3bd0435cdb
Dongyeong Jeong <dyjeong365@gmail.com>
2024-06-26 12:11:59 +0900

    Time: 1 ms (100%), Space: 44.8 MB (26%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.java
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -1,38 +1,25 @@
 class Solution {
     public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        final int MAX = 101;
         List<Integer> ans = new ArrayList<>();
-        int[] nums = new int[101];
-
-        Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>();
-        Set<Integer> set3 = new HashSet<>();
+        boolean[] b1 = new boolean[MAX];
+        boolean[] b2 = new boolean[MAX];
+        boolean[] b3 = new boolean[MAX];
 
         for (var num1 : nums1) {
-            set1.add(num1);
+            b1[num1] = true;
         }
 
         for (var num2 : nums2) {
-            set2.add(num2);
+            b2[num2] = true;
         }
 
         for (var num3 : nums3) {
-            set3.add(num3);
+            b3[num3] = true;
         }
 
-        for (var num1 : set1) {
-            nums[num1]++;
-        }
-
-        for (var num2 : set2) {
-            nums[num2]++;
-        }
-
-        for (var num3 : set3) {
-            nums[num3]++;
-        }
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > 1) {
+        for (int i = 1; i < MAX; i++) {
+            if ((b1[i] && b2[i]) || (b1[i] && b3[i]) || (b2[i] && b3[i])) {
                 ans.add(i);
             }
         }
