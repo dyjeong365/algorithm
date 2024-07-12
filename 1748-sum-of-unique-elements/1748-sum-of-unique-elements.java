38fd0b80d738e8d7caf238d2e8258a3d3a9495a1
Dongyeong Jeong <dyjeong365@gmail.com>
2024-07-12 10:44:54 +0900

    Time: 0 ms (100%), Space: 40.9 MB (87.47%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] arr = new int[101];
+        int sum = 0;
+
+        for (var num : nums) {
+            arr[num]++;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
