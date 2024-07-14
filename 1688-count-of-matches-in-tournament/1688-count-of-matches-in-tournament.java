d5313af13d51eee1378ac51f2fee833501c5cbcd
Dongyeong Jeong <dyjeong365@gmail.com>
2024-07-14 11:03:16 +0900

    Time: 0 ms (100%), Space: 40.6 MB (19.88%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -1,13 +1,5 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int res = 0;
-
-        while (n > 1) {
-            int matches = n / 2;
-            n = matches + n % 2;
-            res += matches;
-        }
-
-        return res;
+        return n - 1;
     }
 }
