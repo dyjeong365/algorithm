75de568194184d164d98279b2d9c28f79a224c07
Dongyeong Jeong <dyjeong365@gmail.com>
2024-07-16 15:31:33 +0900

    Time: 0 ms (100%), Space: 41.6 MB (34.66%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,7 +1,27 @@
 class Solution {
     public String interpret(String command) {
-        return command.replace("()", "o")
-                .replace("(", "")
-                .replace(")", "");
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        while (i < command.length()) {
+            char cur = command.charAt(i);
+
+            if (cur == '(') {
+                if (command.charAt(i + 1) == ')') {
+                    sb.append('o');
+                    i += 2;
+                }
+
+                else if (command.charAt(i + 1) == 'a') {
+                    sb.append("al");
+                    i += 4;
+                }
+            } else {
+                sb.append(cur);
+                i++;
+            }
+        }
+
+        return sb.toString();
     }
 }
