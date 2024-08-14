d8e9de4c167ac7fb4b91e989d5bbed8c7321314c
Dongyeong Jeong <dyjeong365@gmail.com>
2024-08-14 16:06:36 +0900

    Time: 1 ms (77.26%), Space: 45.3 MB (46.66%) - LeetHub

---
+++ 2923-find-champion-i/2923-find-champion-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findChampion(int[][] grid) {
+        final int n = grid[0].length;
+        int[] counts = new int[n];
+        int ans = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    counts[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < counts.length; i++) {
+            if (max < counts[i]) {
+                max = counts[i];
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+}
