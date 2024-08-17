321ca490ad4a5834f6b65368974e5c06578ce9ff
Dongyeong Jeong <dyjeong365@gmail.com>
2024-08-17 18:08:36 +0900

    Time: 1 ms (99.85%), Space: 41.8 MB (62.84%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         int score = 0;
 
         for (int i = 0; i < s.length() - 1; i++) {
-            score += Math.abs((s.charAt(i) - '0') - (s.charAt(i + 1) - '0'));
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
         }
 
         return score;
