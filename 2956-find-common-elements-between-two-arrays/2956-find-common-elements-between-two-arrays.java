214fee6400fbeefe39a13119a7ac03bb2b1b2ad1
Dongyeong Jeong <dyjeong365@gmail.com>
2024-09-19 09:10:01 +0900

    Time: 6 ms (49.68%), Space: 45.5 MB (50.05%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
         int[] ans = new int[2];
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
 
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    ans[0]++;
-                    break;
-                }
+        for (var num1 : nums1) {
+            set1.add(num1);
+        }
+
+        for (var num2 : nums2) {
+            set2.add(num2);
+        }
+
+        for (var num1 : nums1) {
+            if (set2.contains(num1)) {
+                ans[0]++;
             }
         }
 
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = 0; j < nums1.length; j++) {
-                if (nums2[i] == nums1[j]) {
-                    ans[1]++;
-                    break;
-                }
+        for (var num2 : nums2) {
+            if (set1.contains(num2)) {
+                ans[1]++;
             }
         }
 
