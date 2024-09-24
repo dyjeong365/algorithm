2c25899eae4f4a74a9843d6b2bcc312304ed906d
Dongyeong Jeong <dyjeong365@gmail.com>
2024-09-24 09:22:53 +0900

    Time: 0 ms (100%), Space: 42.1 MB (92.1%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -3,8 +3,10 @@ class Solution {
         int seniorCount = 0;
 
         for (var detail : details) {
-            int age = Integer.parseInt(detail.substring(11, 13));
-            
+            int ageTens = detail.charAt(11) - '0';
+            int ageOnes = detail.charAt(12) - '0';
+            int age = ageTens * 10 + ageOnes;
+
             if (age > 60) {
                 seniorCount++;
             }
