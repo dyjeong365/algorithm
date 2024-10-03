15e3e94ad3bf7e69221ed9b9903197a8f9867db1
Dongyeong Jeong <dyjeong365@gmail.com>
2024-10-03 09:21:16 +0900

    Time: 2 ms (58.82%), Space: 43 MB (47.82%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -1,17 +1,8 @@
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-        final double LEN = nums1.length;
-        int sum1 = 0;
-        int sum2 = 0;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
-        for (var num2 : nums2) {
-            sum2 += num2;
-        }
-
-        for (var num1 : nums1) {
-            sum1 += num1;
-        }
-
-        return (int) ((sum2 - sum1) / LEN);
+        return nums2[0] - nums1[0];
     }
 }
