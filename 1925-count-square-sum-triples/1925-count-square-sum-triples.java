fb92e7c770f0807d21f94d164fb94a3735b4c582
Dongyeong Jeong <dyjeong365@gmail.com>
2024-10-21 00:51:15 +0900

    Time: 421 ms (5.18%), Space: 40.4 MB (54.95%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -3,10 +3,10 @@ class Solution {
         int ans = 0;
 
         for (int a = 1; a <= n; a++) {
-            for (int b = 1; b <= n; b++) {
-                for (int c = 1; c <= n; c++) {
+            for (int b = a + 1; b <= n; b++) {
+                for (int c = b + 1; c <= n; c++) {
                     if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
-                        ans++;
+                        ans += 2;
                     }
                 }
             }
