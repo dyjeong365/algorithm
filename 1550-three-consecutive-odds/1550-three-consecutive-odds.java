69204e2afc3d71b9e07c1b8885b122ba740d7f10
Dongyeong Jeong <dyjeong365@gmail.com>
2024-10-22 12:31:38 +0900

    Time: 0 ms (100%), Space: 42.6 MB (16.2%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.java
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         for (int i = 0; i < arr.length - 2; i++) {
-            if (arr[i] % 2 != 0 && arr[i + 1] % 2 != 0 && arr[i + 2] % 2 != 0) {
+            if ((arr[i] * arr[i + 1] * arr[i + 2]) % 2 == 1) {
                 return true;
             }
         }
