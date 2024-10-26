7e27e24b60009125aa454a66609dd2ddd14e4f87
Dongyeong Jeong <dyjeong365@gmail.com>
2024-10-26 22:51:58 +0900

    Time: 1 ms (100%), Space: 43 MB (28.42%) - LeetHub

--- 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
@@ -1,12 +1,8 @@
 class Solution {
     public String getEncryptedString(String s, int k) {
         final int LEN = s.length();
-        StringBuilder sb = new StringBuilder(s);
+        k %= LEN;
 
-        for (int i = 0; i < LEN; i++) {
-            sb.setCharAt(i, s.charAt((i + k) % LEN));
-        }
-
-        return sb.toString();
+        return (s + s).substring(k, k+LEN);
     }
 }
