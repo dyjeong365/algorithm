4d6f871aa3b1c0d7528a3753d1066298e4df52d6
Dongyeong Jeong <dyjeong365@gmail.com>
2024-10-28 22:36:37 +0900

    Time: 0 ms (100%), Space: 40.1 MB (65.28%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean divisorGame(int n) {
-        boolean flag = false;
-
-        while (n > 1) {
-            for (int x = 1; x < n; x++) {
-                if (n % x == 0) {
-                    n -= x;
-                    flag = !flag;
-                    break;
-                }
-            }
-        }
-
-        return flag;
+        return n == 1 ? false : !divisorGame(n - 1);
     }
 }
