995f398b594c48e5864970a995ce037f3b4d40cf
Dongyeong Jeong <dyjeong365@gmail.com>
2024-10-29 10:47:35 +0900

    Time: 0 ms (100%), Space: 42 MB (38.49%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.java
@@ -1,15 +1,8 @@
 class Solution {
     public boolean checkTwoChessboards(String coordinate1, String coordinate2) {
-        boolean[][] chessBoard = new boolean[9][9];
+        int c1 = coordinate1.charAt(0) - 'a' + coordinate1.charAt(1) - '0';
+        int c2 = coordinate2.charAt(0) - 'a' + coordinate2.charAt(1) - '0';   
 
-        for (int x = 1; x < chessBoard.length; x++) {
-            for (int y = x % 2 == 1 ? 1 : 2; y < chessBoard[x].length; y += 2) {
-                chessBoard[x][y] = true;
-            }
-        }
-
-        return chessBoard[coordinate1.charAt(0) - 'a' + 1][Character
-                .getNumericValue(coordinate1.charAt(1))] == chessBoard[coordinate2.charAt(0) - 'a' + 1][Character
-                        .getNumericValue(coordinate2.charAt(1))];
+        return c1 % 2 == c2 % 2;
     }
 }
