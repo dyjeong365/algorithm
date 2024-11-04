af04ef5935941dffbb99070afaab4c24cabf66ec
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-04 21:58:28 +0900

    Time: 1 ms (95.6%), Space: 41.8 MB (75.65%) - LeetHub

--- 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.java
@@ -1,22 +1,17 @@
 class Solution {
     public int minOperations(List<Integer> nums, int k) {
         int res = 0;
-        Set<String> set = new HashSet<>();
-
-        for (int i = 1; i <= k; i++) {
-            set.add(String.valueOf(i));
-        }
+        Set<Integer> set = new HashSet<>();
 
         for (int i = nums.size() - 1; i >= 0; i--) {
-            String target = String.valueOf(nums.get(i));
             res++;
 
-            if (set.contains(target)) {
-                set.remove(target);
+            if (nums.get(i) <= k) {
+                set.add(nums.get(i));
+            }
 
-                if (set.isEmpty()) {
-                    return res;
-                }
+            if (set.size() == k) {
+                return res;
             }
         }
 
