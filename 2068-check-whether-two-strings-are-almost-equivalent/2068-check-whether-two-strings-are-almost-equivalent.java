bd6771cc2c7c6d80cb11f6ddc72250f673f23776
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-05 09:31:00 +0900

    Time: 0 ms (100%), Space: 41.2 MB (97.83%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -1,30 +1,16 @@
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
         final int MAX = 3;
-        final int ALPHABET_NUMBERS = 27;
-        int[] frequencyOfWord1 = new int[ALPHABET_NUMBERS];
-        int[] frequencyOfWord2 = new int[ALPHABET_NUMBERS];
-        Set<Character> set1 = new HashSet<>();
-        Set<Character> set2 = new HashSet<>();
+        final int ALPHABET_NUMBERS = 26;
+        int[] cnt = new int[ALPHABET_NUMBERS];
 
-        for (var pos : word1.toCharArray()) {
-            frequencyOfWord1[pos - 'a']++;
-            set1.add(pos);
+        for (int i = 0; i < word1.length(); i++) {
+            cnt[word1.charAt(i) - 'a']++;
+            cnt[word2.charAt(i) - 'a']--;
         }
 
-        for (var pos : word2.toCharArray()) {
-            frequencyOfWord2[pos - 'a']++;
-            set2.add(pos);
-        }
-
-        for (var pos : set1) {
-            if (Math.abs(frequencyOfWord1[pos - 'a'] - frequencyOfWord2[pos - 'a']) > MAX) {
-                return false;
-            }
-        }
-
-        for (var pos : set2) {
-            if (Math.abs(frequencyOfWord1[pos - 'a'] - frequencyOfWord2[pos - 'a']) > MAX) {
+        for (var pos : cnt) {
+            if (Math.abs(pos) > MAX) {
                 return false;
             }
         }
