630297e64eeb7682927756b9f571534b31fed298
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-05 09:27:46 +0900

    Time: 2 ms (47.65%), Space: 42 MB (15.5%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -4,22 +4,26 @@ class Solution {
         final int ALPHABET_NUMBERS = 27;
         int[] frequencyOfWord1 = new int[ALPHABET_NUMBERS];
         int[] frequencyOfWord2 = new int[ALPHABET_NUMBERS];
+        Set<Character> set1 = new HashSet<>();
+        Set<Character> set2 = new HashSet<>();
 
         for (var pos : word1.toCharArray()) {
             frequencyOfWord1[pos - 'a']++;
+            set1.add(pos);
         }
 
         for (var pos : word2.toCharArray()) {
             frequencyOfWord2[pos - 'a']++;
+            set2.add(pos);
         }
 
-        for (var pos : word1.toCharArray()) {
+        for (var pos : set1) {
             if (Math.abs(frequencyOfWord1[pos - 'a'] - frequencyOfWord2[pos - 'a']) > MAX) {
                 return false;
             }
         }
 
-        for (var pos : word2.toCharArray()) {
+        for (var pos : set2) {
             if (Math.abs(frequencyOfWord1[pos - 'a'] - frequencyOfWord2[pos - 'a']) > MAX) {
                 return false;
             }
