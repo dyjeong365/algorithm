8fac2e5fe5b78bd74590e01a77d35e09b41598ab
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-18 09:13:46 +0900

    Time: 0 ms (100%), Space: 40.6 MB (16.1%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
@@ -1,10 +1,25 @@
 class Solution {
     public int sumBase(int n, int k) {
+        int value = convert(n, k);
         int res = 0;
-        String baseK = Integer.toString(n, k);
 
-        for (var pos : baseK.toCharArray()) {
-            res += (pos - '0');
+        while (value != 0) {
+            res += value % 10;
+            value /= 10;
+        }
+
+        return res;
+    }
+
+    private static int convert(int num, int base) {
+        int pow = 1;
+        int res = 0;
+
+        while (num != 0) {
+            int r = num % base;
+            num /= base;
+            res += r * pow;
+            pow *= 10;
         }
 
         return res;
