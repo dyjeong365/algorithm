0f2a637faea26de0b77601c3bad782311d2ce9c6
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-20 21:46:16 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxPower(String s) {
+        int max = 1;
+        char before = s.charAt(0);
+        int res = 0;
+
+        for (var pos : s.toCharArray()) {
+            if (before == pos) {
+                res++;
+            }
+
+            else {
+                before = pos;
+                max = Math.max(max, res);
+                res = 1;
+            }
+        }
+
+        return max;
+    }
+}
