d53c6182cc8622649025f4c476d80a362b00d43b
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-21 12:12:54 +0900

    Time: 3 ms (81.53%), Space: 42.8 MB (30.66%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] splitedTitle = title.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (var pos : splitedTitle) {
+            if (pos.length() == 1 || pos.length() == 2) {
+                sb.append(pos.toLowerCase());
+            }
+
+            else {
+                sb.append(pos.substring(0, 1).toUpperCase())
+                        .append(pos.substring(1).toLowerCase());
+            }
+
+            sb.append(" ");
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+
+        return sb.toString();
+    }
+}
