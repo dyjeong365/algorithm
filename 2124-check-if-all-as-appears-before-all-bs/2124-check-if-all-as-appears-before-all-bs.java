1b8c1e4d9af11ace81e8ffabe5096ab5faa68d1e
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-23 18:51:40 +0900

    Time: 0 ms (100%), Space: 41.6 MB (37.61%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,15 +1,5 @@
 class Solution {
     public boolean checkString(String s) {
-        char prev = s.charAt(0);
-
-        for (var pos : s.toCharArray()) {
-            if (prev == 'b' && pos == 'a') {
-                return false;
-            }
-
-            prev = pos;
-        }
-
-        return true;
+        return !s.contains("ba");
     }
 }
