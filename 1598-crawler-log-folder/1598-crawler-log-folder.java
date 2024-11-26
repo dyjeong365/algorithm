c333c097ed254d0f4efc7118253a3caaabadb98a
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-26 10:53:24 +0900

    Time: 2 ms (26.43%), Space: 42.2 MB (32.73%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minOperations(String[] logs) {
-        int res = 0;
+        Stack<String> stack = new Stack<>();
 
         for (var log : logs) {
             if (log.equals("../")) {
-                res = Math.max(0, res - 1);
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
             }
 
             else if (!log.equals("./")) {
-                res++;
+                stack.push(log);
             }
         }
 
-        return res;
+        return stack.size();
     }
 }
