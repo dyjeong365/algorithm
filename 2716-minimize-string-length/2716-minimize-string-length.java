18734d32036e2617507e5af6a40b0a001c327221
Dongyeong Jeong <dyjeong365@gmail.com>
2024-11-29 09:50:07 +0900

    Time: 17 ms (5.86%), Space: 45.5 MB (17.15%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.java
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -1,11 +1,5 @@
 class Solution {
     public int minimizedStringLength(String s) {
-        Set<Character> set = new HashSet<>();
-
-        for (var pos : s.toCharArray()) {
-            set.add(pos);
-        }
-
-        return set.size();
+        return (int) s.chars().distinct().count();
     }
 }
