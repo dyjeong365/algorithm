d672762f8b97c4126e2d0658179f0c6842d5c128
Dongyeong Jeong <dyjeong365@gmail.com>
2024-12-01 22:17:26 +0900

    Time: 3 ms (91.65%), Space: 43.7 MB (55.93%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,39 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        int idx = 0;
+        int[] res = new int[nums1.length];
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        /*
-         * 1. 같은 수를 찾는다.
-         * 2. 그 수를 기준으로 오른쪽으로 더 큰 수를 찾는다 (num2.length 까지)
-         * 3. 발견하지 못하면 -1 반환
-         * 4. ans 의 idx++;
-         */
-        for (var num1 : nums1) {
-            for (int i = 0; i < nums2.length; i++) {
-                if (num1 == nums2[i]) {
-                    int j = i + 1;
-
-                    while (j < nums2.length) {
-                        if (num1 < nums2[j]) {
-                            ans[idx] = nums2[j];
-                            break;
-                        }
-
-                        else {
-                            j++;
-                        }
-                    }
-
-                    if (ans[idx] == 0) {
-                        ans[idx] = -1;
-                    }
-
-                    idx++;
-                }
+        for (var num2 : nums2) {
+            while (!stack.isEmpty() && num2 > stack.peek()) {
+                map.put(stack.pop(), num2);
             }
+
+            stack.push(num2);
         }
 
-        return ans;
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
+        }
+
+        return res;
     }
 }
