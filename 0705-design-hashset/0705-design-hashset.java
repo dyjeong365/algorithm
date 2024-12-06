64c53e497481e9103ada225e2a3c3e7ac615c64a
Dongyeong Jeong <dyjeong365@gmail.com>
2024-12-06 17:08:14 +0900

    Time: 14 ms (62.16%), Space: 54.7 MB (17.06%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.java
+++ 0705-design-hashset/0705-design-hashset.java
@@ -1,25 +1,4 @@
-class MyHashSet {
-    List<String> list;
-
-    public MyHashSet() {
-        list = new ArrayList<>();
-    }
-
-    public void add(int key) {
-        if (!list.contains(String.valueOf(key))) {
-            list.add(String.valueOf(key));
-        }
-    }
-
-    public void remove(int key) {
-        if (list.contains(String.valueOf(key))) {
-            list.remove(String.valueOf(key));
-        }
-    }
-
-    public boolean contains(int key) {
-        return list.contains(String.valueOf(key));
-    }
+class MyHashSet extends HashSet<Integer> {
 }
 
 /**
