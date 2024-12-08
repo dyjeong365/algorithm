5b0280a535f1e0d7fd41bd0606e05ba4b9240dcc
Dongyeong Jeong <dyjeong365@gmail.com>
2024-12-08 21:14:19 +0900

    Time: 7 ms (79.8%), Space: 44.2 MB (61.03%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        StringBuilder sb = new StringBuilder();
+        Map<Character, Character> map = new HashMap<>();
+        char alphabet = 'a';
+        char value = 'a';
+
+        for (var pos : key.toCharArray()) {
+            if (pos != ' ' && !map.containsKey(pos)) {
+                map.put(pos, (char) value++);
+            }
+        }
+
+        for (var pos : message.toCharArray()) {
+            sb.append(map.containsKey(pos) ? map.get(pos) : " ");
+        }
+
+        return sb.toString();
+    }
+}
