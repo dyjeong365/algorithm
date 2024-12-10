3638c8823c24e83e1a258258a0b7633f78468bc6
Dongyeong Jeong <dyjeong365@gmail.com>
2024-12-10 22:50:23 +0900

    Time: 1 ms (100%), Space: 45 MB (47.35%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
@@ -1,33 +1,22 @@
 class Solution {
     public List<String> cellsInRange(String s) {
-        List<String> ans = new ArrayList<>();
-        final char MAX_COL = s.charAt(3);
-        final int MAX_ROW = Character.getNumericValue(s.charAt(4));
-        String start = s.substring(0, 2);
-        String end = s.substring(3);
+        List<String> cells = new ArrayList<>();
+        char c1 = s.charAt(0);
+        int r1 = Character.getNumericValue(s.charAt(1));
+        char c2 = s.charAt(3);
+        int r2 = Character.getNumericValue(s.charAt(4));
 
-        while (!start.equals(end)) {
-            StringBuilder sb = new StringBuilder();
-            char col = start.charAt(0);
-            int row = Character.getNumericValue(start.charAt(1));
+        for (char c = c1; c <= c2; c++) {
+            for (int r = r1; r <= r2; r++) {
+                StringBuilder sb = new StringBuilder();
+                
+                sb.append(c)
+                        .append(r);
 
-            ans.add(start);
-
-            if (row < MAX_ROW) {
-                row++;
-            } else {
-                col++;
-                row = Character.getNumericValue(s.charAt(1));
+                cells.add(sb.toString());
             }
-
-            sb.append(col)
-                    .append(String.valueOf(row));
-
-            start = sb.toString();
         }
 
-        ans.add(end);
-
-        return ans;
+        return cells;
     }
 }
