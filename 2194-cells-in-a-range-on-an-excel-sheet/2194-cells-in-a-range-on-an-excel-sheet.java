8a9de3856141042cfe0d07e83d944d812cb22b4e
Dongyeong Jeong <dyjeong365@gmail.com>
2024-12-10 22:45:35 +0900

    Time: 2 ms (87.08%), Space: 44.9 MB (47.35%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
@@ -1,19 +1,15 @@
 class Solution {
     public List<String> cellsInRange(String s) {
         List<String> ans = new ArrayList<>();
-        // StringBuilder sb = new StringBuilder();
         final char MAX_COL = s.charAt(3);
         final int MAX_ROW = Character.getNumericValue(s.charAt(4));
         String start = s.substring(0, 2);
         String end = s.substring(3);
-        // System.out.println("start: " + start);
-        // System.out.println("end: " + end);
 
         while (!start.equals(end)) {
+            StringBuilder sb = new StringBuilder();
             char col = start.charAt(0);
             int row = Character.getNumericValue(start.charAt(1));
-            // System.out.println("col: " + col);
-            // System.out.println("row: " + row);
 
             ans.add(start);
 
@@ -24,8 +20,10 @@ class Solution {
                 row = Character.getNumericValue(s.charAt(1));
             }
 
-            start = col + String.valueOf(row);
-            // System.out.println("start: " + start);
+            sb.append(col)
+                    .append(String.valueOf(row));
+
+            start = sb.toString();
         }
 
         ans.add(end);
