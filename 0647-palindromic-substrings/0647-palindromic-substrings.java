b75505d73f902f106944eac1e0c260ccb169e645
Dongyeong Jeong <dyjeong365@gmail.com>
2024-12-19 10:44:59 +0900

    Time: 742 ms (13.9%), Space: 41.6 MB (60.52%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,28 +1,26 @@
 class Solution {
-    static int len;
-
     public int countSubstrings(String s) {
-        len = s.length();
-        int res = len;
+        final int LEN = s.length();
+        int res = 0;
 
-        for (int end = 2; end <= len; end++) {
-            for (int start = 0; start < len - 1; start++) {
-                // System.out.println("substring: " + s.substring(start, start + end));
-                if (start + end <= len) {
-                    if (isPalindrome(s.substring(start, start + end))) {
-                        res++;
-                    }
+        for (int i = 0; i < LEN; i++) {
+            for (int j = i; j < LEN; j++) {
+                if (isPalindrome(s, i, j)) {
+                    res++;
                 }
-
             }
         }
 
         return res;
     }
 
-    private static boolean isPalindrome(String sub) {
-        StringBuilder sb = new StringBuilder(sub).reverse();
+    private static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
 
-        return sub.equals(sb.toString());
+        return true;
     }
 }
