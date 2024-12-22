8354ebc18cfe0775ce6637b5887c0fae6bd01fb1
Dongyeong Jeong <dyjeong365@gmail.com>
2024-12-22 20:05:02 +0900

    Time: 6 ms (61.11%), Space: 42.5 MB (25.85%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -1,19 +1,14 @@
 class Solution {
     public String greatestLetter(String s) {
-        int[] counts = new int[26];
         Set<Character> set = new HashSet<>();
 
         for (var pos : s.toCharArray()) {
             set.add(pos);
         }
 
-        for (var pos : set) {
-            counts[Character.isUpperCase(pos) ? pos - 'A' : pos - 'a']++;
-        }
-
-        for (int i = counts.length - 1; i >= 0; i--) {
-            if (counts[i] == 2) {
-                return String.valueOf((char) (i + 'A'));
+        for (char c = 'Z'; c >= 'A'; c--) {
+            if (set.contains(c) && set.contains((char) ((c - 'A') + 'a'))) {
+                return String.valueOf(c);
             }
         }
 
