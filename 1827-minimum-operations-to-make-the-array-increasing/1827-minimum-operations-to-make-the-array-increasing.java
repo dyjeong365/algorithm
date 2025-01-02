addd0164e40fbfd5d87cd303deb9b62777288fe4
Dongyeong Jeong <dyjeong365@gmail.com>
2025-01-02 22:22:53 +0900

    Time: 3 ms (90.5%), Space: 45 MB (55.8%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -3,9 +3,14 @@ class Solution {
         int res = 0;
 
         for (int i = 1; i < nums.length; i++) {
-            while (nums[i - 1] >= nums[i]) {
-                nums[i]++;
-                res++;
+            int prev = nums[i - 1];
+            int cur = nums[i];
+
+            if (prev >= cur) {
+                int replacement = prev + 1;
+
+                res += replacement - cur;
+                nums[i] = replacement;
             }
         }
 
