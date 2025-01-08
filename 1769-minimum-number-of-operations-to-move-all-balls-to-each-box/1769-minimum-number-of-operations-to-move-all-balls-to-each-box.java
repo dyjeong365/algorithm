5f1d15a1c3fd14ce49bd861615b34cf6a283660d
Dongyeong Jeong <dyjeong365@gmail.com>
2025-01-09 00:30:59 +0900

    Time: 6 ms (48.32%), Space: 44.8 MB (43.01%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -2,18 +2,19 @@ class Solution {
     public int[] minOperations(String boxes) {
         final int LEN = boxes.length();
         int[] ans = new int[LEN];
-        List<Integer> indexes = new ArrayList<>();
+
+        int ballsToLeft = 0, movesToLeft = 0;
+        int ballsToRight = 0, movesToRight = 0;
 
         for (int i = 0; i < LEN; i++) {
-            if (boxes.charAt(i) == '1') {
-                indexes.add(i);
-            }
-        }
+            ans[i] += movesToLeft;
+            ballsToLeft += Character.getNumericValue(boxes.charAt(i));
+            movesToLeft += ballsToLeft;
 
-        for (int i = 0; i < LEN; i++) {
-            for (var index : indexes) {
-                ans[i] += Math.abs(i - index);
-            }
+            int j = LEN - 1 - i;
+            ans[j] += movesToRight;
+            ballsToRight += Character.getNumericValue(boxes.charAt(j));
+            movesToRight += ballsToRight;
         }
 
         return ans;
