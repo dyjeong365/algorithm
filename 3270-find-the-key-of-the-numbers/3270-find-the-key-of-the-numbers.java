d1c62be7c5d370f5f4ddebee224082a992b25515
Dongyeong Jeong <dyjeong365@gmail.com>
2025-01-28 16:11:26 +0900

    Time: 0 ms (100%), Space: 40.8 MB (75.14%) - LeetHub

--- 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.java
+++ 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.java
@@ -1,8 +1,8 @@
 class Solution {
     public int generateKey(int num1, int num2, int num3) {
         return Math.min(Math.min(num1 / 1000, num2 / 1000), num3 / 1000) * 1000 +
-                Math.min(Math.min(num1 / 100 % 10, num2 / 100 % 10), num3 / 100 % 10) * 100 +
-                Math.min(Math.min(num1 / 10 % 10, num2 / 10 % 10), num3 / 10 % 10) * 10
+                Math.min(Math.min(num1 % 1000 / 100, num2 % 1000 / 100), num3 % 1000 / 100) * 100 +
+                Math.min(Math.min(num1 % 100 / 10, num2 % 100 / 10), num3 % 100 / 10) * 10
                 + Math.min(Math.min(num1 % 10, num2 % 10), num3 % 10);
     }
 }
