6873beeb03bd8c4337c82d0555b7c69a81183583
Dongyeong Jeong <dyjeong365@gmail.com>
2025-01-31 22:59:55 +0900

    Time: 8 ms (1.54%), Space: 43.9 MB (5.13%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -4,10 +4,7 @@ class Solution {
 
         for (int i = 0; i < 31; i++) {
             set.add((int) Math.pow(2, i));
-            set.add((int) Math.pow(-2, i));
         }
-
-        set.add((int) Math.pow(-2, 31));
 
         return set.contains(n);
     }
