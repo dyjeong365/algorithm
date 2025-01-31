1d88474068596a19affc6a1055e33df79b9f14ed
Dongyeong Jeong <dyjeong365@gmail.com>
2025-01-31 23:00:01 +0900

    Time: 1 ms (41.93%), Space: 41 MB (36.58%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        Set<Integer> set = new HashSet<>();
-
         for (int i = 0; i < 31; i++) {
-            set.add((int) Math.pow(2, i));
+            int ans = (int) Math.pow(2, i);
+
+            if (ans == n) {
+                return true;
+            }
         }
 
-        return set.contains(n);
+        return false;
     }
 }
