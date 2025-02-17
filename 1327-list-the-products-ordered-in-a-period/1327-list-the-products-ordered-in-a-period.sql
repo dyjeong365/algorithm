3c6bcf246cbe2084c55e09eb3add6e461af08235
Dongyeong Jeong <dyjeong365@gmail.com>
2025-02-17 15:05:20 +0900

    Time: 1023 ms (79.6%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -3,6 +3,6 @@ SELECT P.product_name, SUM(O.unit) 'unit'
 FROM Products P
 JOIN Orders O
 ON P.product_id = O.product_id
-WHERE O.order_date LIKE '2020-02%'
+WHERE YEAR(O.order_date) = '2020' AND MONTH(O.order_date) = '02'
 GROUP BY P.product_id
 HAVING SUM(O.unit) >= 100;
