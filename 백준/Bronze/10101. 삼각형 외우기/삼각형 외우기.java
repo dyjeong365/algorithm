373a3fd2b52df499e4bb8c3fe5bdd43fb8d878d1
Dongyeong Jeong <dyjeong365@gmail.com>
2025-03-09 11:40:55 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 104 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -1,36 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final int ANGLES = 3;
-        int[] triangle = new int[ANGLES];
+        StringBuilder sb = new StringBuilder();
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int sum = a + b + c;
 
-        for (int i = 0; i < ANGLES; i++) {
-            triangle[i] = Integer.parseInt(br.readLine());
-        }
-
-        final int SUM = Arrays.stream(triangle).sum();
-
-        if (triangle[0] == triangle[1] && triangle[0] == triangle[2]) {
-            System.out.println("Equilateral");
-        } 
-        
-        else if (SUM == 180) {
-            if (Arrays.stream(triangle).distinct().count() == 2) {
-                System.out.println("Isosceles");
-            } 
-            
-            else {
-                System.out.println("Scalene");
+        if (sum == 180) {
+            if (a == 60 && a == b && a == c) {
+                sb.append("Equilateral");
+            } else if (a == b || a == c || b == c) {
+                sb.append("Isosceles");
+            } else {
+                sb.append("Scalene");
             }
-        } 
-        
-        else {
-            System.out.println("Error");
+        } else {
+            sb.append("Error");
         }
+
+        System.out.print(sb);
     }
 }
