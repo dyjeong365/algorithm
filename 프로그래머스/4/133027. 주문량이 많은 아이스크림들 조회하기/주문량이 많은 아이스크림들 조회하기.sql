1f6b1d2293f189ff81da86b981d3c8b18337215e
Dongyeong Jeong <dyjeong365@gmail.com>
2025-03-19 10:11:36 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,12 +1,14 @@
 -- 7월 아이스크림 총 주문량과 상반기의 아이스크림 총 주문량을 더한 값이 큰 순서대로 상위 3개의 맛을 조회하는 SQL 문을 작성해주세요.
 SELECT FLAVOR
 FROM    (
-            SELECT *
+            SELECT FLAVOR, SUM(TOTAL_ORDER) 'TOTAL_ORDERS'
             FROM FIRST_HALF
+            GROUP BY FLAVOR
             UNION ALL
-            SELECT *
+            SELECT FLAVOR, SUM(TOTAL_ORDER)
             FROM JULY
-        ) J
+            GROUP BY FLAVOR
+        ) COMBINED
 GROUP BY FLAVOR
-ORDER BY SUM(TOTAL_ORDER) DESC
+ORDER BY SUM(TOTAL_ORDERS) DESC
 LIMIT 3;
