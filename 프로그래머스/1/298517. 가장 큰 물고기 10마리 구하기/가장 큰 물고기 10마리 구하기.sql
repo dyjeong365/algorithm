36ec2a0a6415e7452e13e23216b62f61c5bb3707
Dongyeong Jeong <dyjeong365@gmail.com>
2025-03-20 11:43:07 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -4,6 +4,9 @@ FISH_INFO 테이블에서 가장 큰 물고기 10마리의 ID와 길이를 출�
 ID 컬럼명은 ID, 길이 컬럼명은 LENGTH로 해주세요.
 */
 SELECT ID, LENGTH
-FROM FISH_INFO
-ORDER BY LENGTH DESC, ID
-LIMIT 10;
+FROM    (
+            SELECT ID, LENGTH, ROW_NUMBER() OVER(ORDER BY LENGTH DESC) 'rnk'
+            FROM FISH_INFO
+        ) I
+WHERE rnk <= 10
+ORDER BY LENGTH DESC, ID;
