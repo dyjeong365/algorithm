8951cecc5255b39fe74b49bd427fbce93cb90f48
Dongyeong Jeong <dyjeong365@gmail.com>
2025-03-23 11:56:27 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -6,21 +6,18 @@ HR_DEPARTMENT, HR_EMPLOYEES, HR_GRADE 테이블을 이용해 사원별 성과금
 */
 SELECT E.EMP_NO, EMP_NAME,
     CASE 
-        WHEN SCORE >= 96 THEN 'S'
-        WHEN SCORE >= 90 THEN 'A'
-        WHEN SCORE >= 80 THEN 'B'
+        WHEN AVG(SCORE) >= 96 THEN 'S'
+        WHEN AVG(SCORE) >= 90 THEN 'A'
+        WHEN AVG(SCORE) >= 80 THEN 'B'
         ELSE 'C'
     END 'GRADE', 
     CASE 
-        WHEN SCORE >= 96 THEN SAL * 0.2
-        WHEN SCORE >= 90 THEN SAL * 0.15
-        WHEN SCORE >= 80 THEN SAL * 0.1
+        WHEN AVG(SCORE) >= 96 THEN SAL * 0.2
+        WHEN AVG(SCORE) >= 90 THEN SAL * 0.15
+        WHEN AVG(SCORE) >= 80 THEN SAL * 0.1
         ELSE 0
     END 'BONUS'
-FROM HR_EMPLOYEES E, (
-                        SELECT EMP_NO, AVG(SCORE) 'SCORE'
-                        FROM HR_GRADE
-                        GROUP BY EMP_NO
-                     ) G
+FROM HR_EMPLOYEES E, HR_GRADE G                     
 WHERE E.EMP_NO = G.EMP_NO
+GROUP BY E.EMP_NO
 ORDER BY E.EMP_NO;
