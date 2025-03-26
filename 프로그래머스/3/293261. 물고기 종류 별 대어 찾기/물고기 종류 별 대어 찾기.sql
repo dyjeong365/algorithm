7062a8e89ef00c0a3aa6560132830f86f7e72ef9
Dongyeong Jeong <dyjeong365@gmail.com>
2025-03-26 11:00:57 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,15 @@
+/*
+물고기 종류 별로 가장 큰 물고기의 ID, 물고기 이름, 길이를 출력하는 SQL 문을 작성해주세요.
+
+물고기의 ID 컬럼명은 ID, 이름 컬럼명은 FISH_NAME, 길이 컬럼명은 LENGTH로 해주세요.
+결과는 물고기의 ID에 대해 오름차순 정렬해주세요.
+단, 물고기 종류별 가장 큰 물고기는 1마리만 있으며 10cm 이하의 물고기가 가장 큰 경우는 없습니다.
+*/
+SELECT ID, FISH_NAME, LENGTH
+FROM    (
+            SELECT ID, FISH_TYPE, LENGTH, RANK() OVER(PARTITION BY FISH_TYPE ORDER BY LENGTH DESC) 'rnk'
+            FROM FISH_INFO
+        ) I
+        , FISH_NAME_INFO NI
+WHERE I.FISH_TYPE = NI.FISH_TYPE AND rnk = 1
+ORDER BY ID;
