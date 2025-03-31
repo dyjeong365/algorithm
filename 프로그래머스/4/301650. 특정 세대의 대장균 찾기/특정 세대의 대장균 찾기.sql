c42d1d2d9c56be8622fcc1ae3e71987c95162895
Dongyeong Jeong <dyjeong365@gmail.com>
2025-03-31 09:52:34 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,12 +1,14 @@
 -- 3세대의 대장균의 ID(ID) 를 출력하는 SQL 문을 작성해주세요. 이때 결과는 대장균의 ID 에 대해 오름차순 정렬해주세요.
-SELECT 3G.ID
-FROM    (
-            SELECT *
-            FROM ECOLI_DATA
-            WHERE PARENT_ID IS NULL
-        ) 1G
-        , ECOLI_DATA 2G
-        , ECOLI_DATA 3G
-WHERE 1G.ID = 2G.PARENT_ID 
-AND 2G.ID = 3G.PARENT_ID
-ORDER BY 3G.ID;
+WITH RECURSIVE TBL AS   (
+                            SELECT ID, PARENT_ID, 1 'GENERATION'
+                            FROM ECOLI_DATA
+                            WHERE PARENT_ID IS NULL
+                            UNION
+                            SELECT E.ID, E.PARENT_ID, GENERATION + 1
+                            FROM ECOLI_DATA E, TBL T
+                            WHERE E.PARENT_ID = T.ID
+                        )
+SELECT ID
+FROM TBL
+WHERE GENERATION = 3
+ORDER BY ID;
