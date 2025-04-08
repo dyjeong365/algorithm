4198547c7f1b27e3d6d99db53eef6c479c7d9808
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-08 09:34:26 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,26 @@
+/*
+DEVELOPERS 테이블에서 GRADE별 개발자의 정보를 조회하려 합니다. GRADE는 다음과 같이 정해집니다.
+
+A : Front End 스킬과 Python 스킬을 함께 가지고 있는 개발자
+B : C# 스킬을 가진 개발자
+C : 그 외의 Front End 개발자
+GRADE가 존재하는 개발자의 GRADE, ID, EMAIL을 조회하는 SQL 문을 작성해 주세요.
+
+결과는 GRADE와 ID를 기준으로 오름차순 정렬해 주세요.
+*/
+WITH TBL AS     (
+                    SELECT DISTINCT CASE
+                                        WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') > 0 AND CATEGORY = 'Front End' THEN 'A'
+                                        WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') > 0 THEN 'B'
+                                        WHEN CATEGORY = 'Front End' THEN 'C'
+                                    END 'GRADE'
+    , ID
+    , EMAIL
+                    FROM SKILLCODES, DEVELOPERS
+                    WHERE SKILL_CODE & CODE = CODE
+                )
+
+SELECT *
+FROM TBL
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE, ID;
