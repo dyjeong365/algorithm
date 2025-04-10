2cb6296ef0a379edc786d8b127f965a9e159d89e
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-11 08:33:44 +0900

    Time: 930 ms (84.64%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -4,10 +4,14 @@ Write a solution to find the number of times each student attended each exam.
 Return the result table ordered by student_id and subject_name.
 */
 
-SELECT S.student_id, S.student_name, J.subject_name, COUNT(E.student_id) 'attended_exams'
+SELECT S.student_id, S.student_name, J.subject_name, COALESCE(attended_exams, 0) 'attended_exams'
 FROM Students S
 CROSS JOIN Subjects J
-LEFT JOIN Examinations E
+LEFT JOIN 
+    (
+        SELECT student_id, subject_name, COUNT(*) 'attended_exams'
+        FROM Examinations
+        GROUP BY student_id, subject_name
+    ) E
 ON S.student_id = E.student_id AND E.subject_name = J.subject_name
-GROUP BY S.student_id, J.subject_name
 ORDER BY S.student_id, J.subject_name;
