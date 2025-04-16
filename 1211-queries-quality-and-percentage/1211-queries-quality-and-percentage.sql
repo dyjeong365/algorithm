9afc6038d57bd1b79da95fea69a6244b4623f269
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-16 12:47:25 +0900

    Time: 371 ms (78.93%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,22 @@
+/*
+We define query quality as:
+The average of the ratio between query rating and its position.
+
+We also define poor query percentage as:
+The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+*/
+SELECT query_name, ROUND(AVG(rating / position), 2) 'quality'
+    , ROUND(
+                AVG(  
+                    CASE 
+                        WHEN rating < 3 THEN 1
+                        ELSE 0
+                    END
+                    )  
+                * 100, 2) 'poor_query_percentage'
+FROM Queries
+GROUP BY query_name;
