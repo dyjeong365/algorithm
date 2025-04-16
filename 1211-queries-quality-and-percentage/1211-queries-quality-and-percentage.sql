c2b3665b6903692cfb9dc52736b5dd4104257452
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-16 12:53:05 +0900

    Time: 418 ms (65.39%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -11,7 +11,7 @@ Both quality and poor_query_percentage should be rounded to 2 decimal places.
 */
 SELECT query_name, ROUND(AVG(rating / position), 2) 'quality'
     , ROUND(
-                AVG(rating < 3) * 100, 2
+                SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2
             ) 'poor_query_percentage'
 FROM Queries
 GROUP BY query_name;
