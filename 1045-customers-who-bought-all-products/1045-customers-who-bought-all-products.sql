8c770f86e7c69739a91940241b4d572bd2b5efd3
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-17 18:36:31 +0900

    Time: 596 ms (74.05%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,5 @@
 # Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
 SELECT customer_id
 FROM Customer
-WHERE product_key IN (SELECT product_key FROM Product)
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
+HAVING COUNT(DISTINCT product_key) >= (SELECT COUNT(*) FROM Product);
