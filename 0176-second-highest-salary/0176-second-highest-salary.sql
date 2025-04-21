1dd7a6ad0ca037b9e92dbc556947a8d2e8753efd
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-21 22:26:14 +0900

    Time: 275 ms (81.17%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,7 +1,9 @@
 # Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null
-SELECT IF(COUNT(*) > 0, salary, null) 'SecondHighestSalary'
-FROM (
-    SELECT id, salary, DENSE_RANK() OVER(ORDER BY salary DESC) 'rnk' 
-    FROM Employee
-) E
-WHERE rnk = 2;
+SELECT 
+    (
+        SELECT distinct salary 
+        FROM Employee 
+        ORDER BY salary DESC 
+        LIMIT 1 
+        OFFSET 1
+    ) 'SecondHighestSalary';
