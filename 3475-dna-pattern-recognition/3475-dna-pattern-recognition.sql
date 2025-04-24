584367bb9e3d9fb83b81ec1a1c665efae8d3b909
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-24 10:59:16 +0900

    Time: 311 ms (66.92%), Space: 0B (100%) - LeetHub

--- 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -7,12 +7,12 @@ Sequences containing the motif ATAT (a simple repeated pattern)
 Sequences that have at least 3 consecutive G (like GGG or GGGG)
 Return the result table ordered by sample_id in ascending order.
 */
-SELECT sample_id
-    , dna_sequence
-    , species
-    , IF(dna_sequence LIKE 'ATG%', 1, 0) 'has_start'
-    , IF(dna_sequence LIKE '%TAA' OR dna_sequence LIKE '%TAG' OR dna_sequence LIKE '%TGA', 1, 0) 'has_stop'
-    , IF(dna_sequence LIKE '%ATAT%', 1, 0) 'has_atat'
-    , IF(dna_sequence LIKE '%GGG%', 1, 0) 'has_ggg'
+SELECT sample_id,
+    dna_sequence,
+    species, 
+    dna_sequence REGEXP '^ATG' AS has_start, 
+    dna_sequence REGEXP 'TAA$|TAG$|TGA$' AS has_stop, 
+    dna_sequence REGEXP 'ATAT' AS has_atat, 
+    dna_sequence REGEXP 'GGG' AS has_ggg
 FROM Samples
 ORDER BY sample_id;
