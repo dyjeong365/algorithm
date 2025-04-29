244ae78acf673a73bb2972d3743af4044d44258e
Dongyeong Jeong <dyjeong365@gmail.com>
2025-04-29 11:08:39 +0900

    Time: 600 ms (49.24%), Space: 0B (100%) - LeetHub

--- 1179-reformat-department-table/1179-reformat-department-table.sql
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -1,16 +1,70 @@
 # Reformat the table such that there is a department id column and a revenue column for each month.
-SELECT id,
-    SUM(IF(month = 'Jan', revenue, null)) 'Jan_Revenue',
-    SUM(IF(month = 'Feb', revenue, null)) 'Feb_Revenue',
-    SUM(IF(month = 'Mar', revenue, null)) 'Mar_Revenue',
-    SUM(IF(month = 'Apr', revenue, null)) 'Apr_Revenue',
-    SUM(IF(month = 'May', revenue, null)) 'May_Revenue',
-    SUM(IF(month = 'Jun', revenue, null)) 'Jun_Revenue',
-    SUM(IF(month = 'Jul', revenue, null)) 'Jul_Revenue',
-    SUM(IF(month = 'Aug', revenue, null)) 'Aug_Revenue',
-    SUM(IF(month = 'Sep', revenue, null)) 'Sep_Revenue',
-    SUM(IF(month = 'Oct', revenue, null)) 'Oct_Revenue',
-    SUM(IF(month = 'Nov', revenue, null)) 'Nov_Revenue',
-    SUM(IF(month = 'Dec', revenue, null)) 'Dec_Revenue'
-FROM Department
-GROUP BY id;
+SELECT
+  Ids.id,
+  January.revenue AS Jan_Revenue,
+  Feburary.revenue AS Feb_Revenue,
+  March.revenue AS Mar_Revenue,
+  April.revenue AS Apr_Revenue,
+  May.revenue AS May_Revenue,
+  June.revenue AS Jun_Revenue,
+  July.revenue AS Jul_Revenue,
+  August.revenue AS Aug_Revenue,
+  September.revenue AS Sep_Revenue,
+  October.revenue AS Oct_Revenue,
+  November.revenue AS Nov_Revenue,
+  December.revenue AS Dec_Revenue
+FROM
+  (
+    SELECT DISTINCT
+      id
+    FROM
+      Department
+  ) AS Ids
+  LEFT JOIN Department AS January ON (
+    Ids.id = January.id
+    AND January.month = "Jan"
+  )
+  LEFT JOIN Department AS Feburary ON (
+    Ids.id = Feburary.id
+    AND Feburary.month = "Feb"
+  )
+  LEFT JOIN Department AS March ON (
+    Ids.id = March.id
+    AND March.month = "Mar"
+  )
+  LEFT JOIN Department AS April ON (
+    Ids.id = April.id
+    AND April.month = "Apr"
+  )
+  LEFT JOIN Department AS May ON (
+    Ids.id = May.id
+    AND May.month = "May"
+  )
+  LEFT JOIN Department AS June ON (
+    Ids.id = June.id
+    AND June.month = "Jun"
+  )
+  LEFT JOIN Department AS July ON (
+    Ids.id = July.id
+    AND July.month = "Jul"
+  )
+  LEFT JOIN Department AS August ON (
+    Ids.id = August.id
+    AND August.month = "Aug"
+  )
+  LEFT JOIN Department AS September ON (
+    Ids.id = September.id
+    AND September.month = "Sep"
+  )
+  LEFT JOIN Department AS October ON (
+    Ids.id = October.id
+    AND October.month = "Oct"
+  )
+  LEFT JOIN Department AS November ON (
+    Ids.id = November.id
+    AND November.month = "Nov"
+  )
+  LEFT JOIN Department AS December ON (
+    Ids.id = December.id
+    AND December.month = "Dec"
+  );
